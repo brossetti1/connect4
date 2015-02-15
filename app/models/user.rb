80988251e9a95a1ea41d0773b4a2fb8c4fd2327d
Vedika Birla <birla22v@mtholyoke.edu>
2015-02-14 22:07:07 -0500

    everything besides diagonal down

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true
-  
+
   has_one :user_profile
   has_many :games
-  
-  
 
-  
   has_many :players
   has_many :games, through: :players
 
+  def prompt_user(prompt, validator, error_msg, clear_screen: nil)
+    `clear` if clear_screen
+    puts "\n#{prompt}\n"
+    result = gets.chomp
+    until result =~ validator
+      puts "\n#{error_msg}\n"
+      result = gets.chomp
+    end
+    puts
+    result
+  end
+
 end
-
-
-
